ef9c45a07f04ab1d9c15bdd7765fbce0f955c94f
Jon Chen <jchen2190@gmail.com>
2023-05-14 22:42:15 -0400

    express session

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const connectToMongoDB = require("./database/mongodb");
 const cors = require("cors");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
-// const sessions = require("express-session"); // store on server
-const sessions = require('cookie-session'); // store on client
+const sessions = require("express-session"); // store on server
+// const sessions = require('cookie-session'); // store on client
 require("dotenv").config();
 
 app.use(logger("dev")); 
@@ -13,8 +13,8 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 app.use(cors({
-    // "origin": "http://localhost:3000",
-    "origin": "https://jchen2190.github.io/",
+    "origin": "http://localhost:3000",
+    // "origin": "https://jchen2190.github.io/",
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "credentials": true,
     "optionSuccessStatus": 200
@@ -24,12 +24,12 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 const oneDay = 1000 * 60 * 60 * 24
 app.use(sessions({
     secret: process.env.COOKIE_SECRET,
-    saveUninitialized: true,
+    saveUninitialized: false,
+    resave: false,
     cookie: {
         maxAge: oneDay,
         secure: false
-        },
-    resave: true,
+    },
 }))
 
 const taskRouter = require("./routes/taskRouter");
